d052bb076cdc4456dbb8a8c99c19dc93d9b1fb56
Кристина Афонина <kristinaafonina@MacBook-Pro-Kristina.local>
2019-12-30 16:34:32 +0300

    Hello

--- execute_bin.c
+++ execute_bin.c
@@ -12,60 +12,49 @@
 
 #include "minishell.h"
 
-int			check_bunfile_sup(char *filepath, char *temp, char *filename)
+int			check_bunfile_sup(char *filename)
 {
-	if (filepath)
-		free(filepath);
-	if (temp)
-		free(temp);
-	ft_putstr("./");
 	ft_putstr(filename);
 	write(1, "\n", 1);
 	return (0);
 }
 
-int			check_binfile(char *filename, char *path)
+int			check_binfile(char *filename)
 {
-	char		*filepath;
 	struct stat	fstat;
-	char		*temp;
 
-	temp = ft_strjoin(path, "/");
-	filepath = ft_strjoin(temp, filename);
-	stat(filepath, &fstat);
-	if (access(filepath, F_OK))
+	stat(filename, &fstat);
+	if (access(filename, F_OK))
 	{
 		ft_putstr("minishell: no such file or directory: ");
-		return (check_bunfile_sup(filepath, temp, filename));
+		return (check_bunfile_sup(filename));
 	}
 	else if (S_ISDIR(fstat.st_mode))
 	{
 		ft_putstr("minishell: is a directory: ");
-		return (check_bunfile_sup(filepath, temp, filename));
+		return (check_bunfile_sup(filename));
 	}
-	else if (access(filepath, X_OK))
+	else if (access(filename, X_OK))
 	{
 		ft_putstr("minishell: permission denied: ");
-		return (check_bunfile_sup(filepath, temp, filename));
+		return (check_bunfile_sup(filename));
 	}
-	free(filepath);
-	free(temp);
 	return (1);
 }
 
 void		execute_bin(char **call, char **env, char **new_env)
 {
-	if (ft_strncmp(call[0], "./", 2) == 0)
+	if (ft_strchr(call[0], '/') != 0)
 	{
-		if (check_binfile(&(call[0][2]), "./") == 0)
+		if (check_binfile(call[0]) == 0)
 			return ;
 		if (fork() == 0)
-			execve(&(call[0][2]), call + 1, new_env);
+			execve(call[0], call, new_env);
 		else
 			wait(NULL);
 		return ;
 	}
-	if (execute_sysbin(call, env, new_env) == 0)
+	else if (execute_sysbin(call, env, new_env) == 0)
 	{
 		ft_putstr("minishell: command not found: ");
 		ft_putstr(call[0]);
